4533e7cf677d12b1f28320cd4c7dc2a8f716d878
AndreaBarrera28 <112991492+AndreaBarrera28@users.noreply.github.com>
2025-04-11 20:40:34 -0600

    Certificado de seguridad

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': 'http://127.0.0.1:8000', // Redirige las solicitudes de /api a tu backend de Laravel
+import { defineConfig, loadEnv } from "vite";
+import plugin from '@vitejs/plugin-react'
+import fs from "fs";
+import * as https from "https";
+
+export default ({ mode }: { mode: string }) => {
+    Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+    const options: https.ServerOptions = {
+        cert: process.env.SSL_CRT_FILE
+            ? fs.readFileSync(process.env.SSL_CRT_FILE)
+            : "",
+        key: process.env.SSL_KEY_FILE
+            ? fs.readFileSync(process.env.SSL_KEY_FILE)
+            : "",
+    };
+    // https://vitejs.dev/config/
+    return defineConfig({
+        plugins: [plugin()],
+        // plugins: [react()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
         },
-    },
-})
+        server: {
+            proxy: {
+                "/api": {
+                    target: 'http://127.0.0.1:8000', // Redirige las solicitudes de /api a tu backend de Laravel
+                    secure: false,
+                    changeOrigin: true,
+                    //rewrite: (path) => path.replace(/^\/api/, ""), //esto no ... da error
+                },
+            },
+            port: 5173,
+            https: options,
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: "static/js/[name].js", // Custom name for entry files
+                    chunkFileNames: "static/js/[name].js", // Custom name for chunks
+                    assetFileNames: "static/assets/[name].[ext]", // Custom name for assets
+                },
+            },
+        },
+        css: {
+            devSourcemap: false,
+          },
+    });
+};
+
